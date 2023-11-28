b0ea12e9743cc43ddee9f6f3d8b835bfcdb44b5f
no-humans <pranavprasad242@gmail.com>
2023-11-28 13:23:14 +0530

    data service file created

---
+++ index.js
@@ -0,0 +1,62 @@
+// import dataservice file from service folder
+const dataservice = require("./service/dataservice");
+
+// to import express
+
+const express = require("express");
+
+// create app
+
+const app = express();
+
+// To covert json data
+
+app.use(express.json());
+
+// request
+
+// register
+
+app.post("/register", (req, res) => {
+  const result = dataservice.register(
+    req.body.uname,
+    req.body.acno,
+    req.body.psw
+  )
+  res.status(result.statusCode).json(result)
+  // if (result){
+  //   res.send("Registration  success")
+  // }
+  // else{
+  //   res.send("user already exist")
+  // }
+  // res.send("success");
+  // console.log(req.body);
+});
+
+// login
+// deposit
+// withdraw
+// transaction history
+// delete
+
+// GET
+
+// app.get("", (req, res) => res.send("get method checking......"));
+
+// // post
+// app.post("", (req, res) => res.send("post method checking......"));
+
+// // put
+// app.put("", (req, res) => res.send("put method checking......"));
+
+// // patch
+// app.patch("", (req, res) => res.send("patch method checking......"));
+
+// // delete
+// app.delete("", (req, res) => res.send("delete method checking......"));
+
+// set port
+app.listen(3000, () => {
+  console.log("server started at port number 3000");
+});
---
+++ service/dataservice.js
@@ -0,0 +1,60 @@
+  userDetails = {
+    1000: {
+      acno: 1000,
+      username: 'anu',
+      password: 123,
+      balance: 0,
+      transaction: [],
+    },
+    1001: {
+      acno: 1001,
+      username: 'john',
+      password: 123,
+      balance: 0,
+      transaction: [],
+    },
+    1002: {
+      acno: 1002,
+      username: 'Rengoku',
+      password: 123,
+      balance: 0,
+      transaction: [],
+    },
+    1003: {
+      acno: 1003,
+      username: 'wick',
+      password: 123,
+      balance: 0,
+      transaction: [],
+    },
+  };
+
+// register
+
+register=(uname, acno, psw)=> {
+    if (acno in userDetails) {
+      return {
+        statusCode:401,
+        status:false,
+        message:"user already exist"
+      }
+    } else {
+      userDetails[acno] = {
+        acno,
+        username: uname,
+        password: psw,
+        balance: 0,
+        transaction: [],
+      };
+      console.log(userDetails);
+      return {
+        statusCode:200,
+        status:true,
+        message:"Registration success"
+      }
+    }
+  }
+
+module.exports={
+  register
+}
